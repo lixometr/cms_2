65bc16130c0f24efc6706e0cda1c26111abea6ab
Yaroslav <lixometr@mail.ru>
2021-03-29 20:40:46 +0300

    fix

--- api/src/config/modules/db.ts
+++ api/src/config/modules/db.ts
@@ -2,6 +2,6 @@ export default {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     name: process.env.DB_NAME,
-    port: 3306,
+    port: 3307,
     host: process.env.DB_HOST ||  'localhost'
 }
