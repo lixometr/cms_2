971af24cadc3dece13b969aafd547d10c81e2f58
Nurik1991 <iiepapx@gmail.com>
2021-04-04 19:02:57 +0300

    basket-favorite

--- frontend/pages/favourite.vue
+++ frontend/pages/favourite.vue
@@ -1,16 +1,23 @@
 <template>
-  <section class="basket">
-    <h2>В избранном <span class="basket__cout">{{cnt}}</span> товара</h2>
+  <section class="basket basket-favorite">
+    <h2>
+      В избранном <span class="basket__cout">{{ cnt }}</span> товара
+    </h2>
     <div class="container">
       <!-- блок с продуктами -->
       <div class="basket__products">
-        
-       <ProductCard type="favourite" v-for="(item, idx) in items" :key="idx" :idx="idx" :product="item"/>
+        <ProductCard
+          type="favourite"
+          v-for="(item, idx) in items"
+          :key="idx"
+          :idx="idx"
+          :product="item"
+        />
       </div>
       <!-- конец блок с продуктами -->
 
       <!-- Блок с информацией о цене и заказе -->
-     
+
       <!-- Конец  Блок с информацией о цене и заказе-->
     </div>
   </section>
@@ -18,17 +25,16 @@
 
 <script>
 export default {
-    computed: {
-        items() {
-            return this.$store.getters['favourite/items']
-        },
-        cnt() {
-            return this.$store.getters['favourite/cnt']
-        }
-    }
+  computed: {
+    items() {
+      return this.$store.getters["favourite/items"];
+    },
+    cnt() {
+      return this.$store.getters["favourite/cnt"];
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-
 </style>
